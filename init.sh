796cc180575df99b3c89768cd612bc11d2f2939e
siva kommuri <siva@kommuri.com>
2014-01-19 19:29:58 -0800

    updating init.sh

--- init.sh
+++ init.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-ANCHOR="~/.personal"
+ANCHOR="${HOME}/.personal"
 
-ln -fs ${ANCHOR} `pwd` 
+# ln -fs ${ANCHOR} `pwd` 
 
-ln -fs ${ANCHOR}/git/gitconfig ~/.gitconfig
-ln -fs ${ANCHOR}/git/gitignore_global ~/.gitignore_global
+rm -f ~/.gitconfig && ln -vs ${ANCHOR}/git/gitconfig ~/.gitconfig
+rm -f ~/.gitignore_global && ln -vs ${ANCHOR}/git/gitignore_global ~/.gitignore_global
 
-if grep -q .personal ~/.bashrc ; then 
+if grep -q ${ANCHOR} ~/.bashrc ; then 
 	echo 'bashrc already installed'; 
 else
 	echo "installing bashrc" 
 	mv ~/.bashrc ~/.bashrc_temp
-	echo ". ${ANCHOR}/bashrc" > ~/.bashrc
+	echo ". ${ANCHOR}/bashrc.sh" > ~/.bashrc
 	cat ~/.bashrc_temp >> ~/.bashrc
 	rm ~/.bashrc_temp
 fi
 
 # install workflows
 sudo rm -rf ~/Library/Services
-ln -fs "${ANCHOR}/mac_os_x/workflow" ~/Library/Services
+ln -vs "${ANCHOR}/mac_os_x/workflow" ~/Library/Services
