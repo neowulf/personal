882f3014a4b559aa77156205fab89b26fa5899d3
siva kommuri <siva@kommuri.com>
2014-03-31 14:57:24 -0700

    updating platform detection

--- bashrc/functions.sh
+++ bashrc/functions.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-PLATFORM=${PLATFORM:-macosx}
 CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PERSONAL_DIR="$(cd "${CUR_DIR}" && cd .. && pwd )"
 
@@ -9,40 +8,40 @@ PERSONAL_DIR="$(cd "${CUR_DIR}" && cd .. && pwd )"
 ###############################
 
 function videa() {
-	if [[ $platform == 'linux' ]]; then
-		idea `pwd`/$1
-	elif [[ $platform == 'macosx' ]]; then
-		${HOME}/Applications/IntelliJ\ IDEA\ 13.app/Contents/MacOS/idea `pwd`/${1}
-	fi
+    if [ $(uname) == "Darwin" ]; then
+        ${HOME}/Applications/IntelliJ\ IDEA\ 13.app/Contents/MacOS/idea `pwd`/${1}
+    else
+        idea `pwd`/$1
+    fi
 }
 
 function parse_git_branch_and_add_brackets {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \[\1\]/'
+    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\ \[\1\]/'
 }
 
 function date_txt {
-  date +%H%M.%S
+    date +%H%M.%S
 }
 
 function memcache_flush_all {
-  echo "flush_all" | nc localhost 11211
+    echo "flush_all" | nc localhost 11211
 }
 
 function killtcs {
-  ps -ef | \grep Bootstrap | \grep -v 'grep' | awk '{print $2}' | xargs kill -9
+    ps -ef | \grep Bootstrap | \grep -v 'grep' | awk '{print $2}' | xargs kill -9
 }
 
 function od {
-	if [[ $platform == 'linux' ]]; then
-	  diff $1 .svn/text-base/$1.svn-base;
-	elif [[ $platform == 'macosx' ]]; then
-	  opendiff $1 .svn/text-base/$1.svn-base;
+    if [ $(uname) == "Darwin" ]; then
+      opendiff $1 .svn/text-base/$1.svn-base;
+	else
+      diff $1 .svn/text-base/$1.svn-base;
 	fi
 }
 
 function restartvpn {
-  sudo launchctl stop com.apple.racoon
-  sudo launchctl start com.apple.racoon
+    sudo launchctl stop com.apple.racoon
+    sudo launchctl start com.apple.racoon
 }
 
 ###############################
@@ -63,14 +62,14 @@ function marks {
     ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
 }
 _completemarks() {
-  local curw=${COMP_WORDS[COMP_CWORD]}
-  if [[ $platform == 'linux' ]]; then
-	  local wordlist=$(find $MARKPATH -type l -printf "%f\n")
-  elif [[ $platform == 'macosx' ]]; then
-	  local wordlist=$(find $MARKPATH -type l -print0 | xargs -0 | \sed 's/\/Users\/siva\/.marks\///g')
-  fi
-  COMPREPLY=($(compgen -W '${wordlist[@]}' -- "$curw"))
-  return 0
+    local curw=${COMP_WORDS[COMP_CWORD]}
+    if [ $(uname) == "Darwin" ]; then
+      local wordlist=$(find $MARKPATH -type l -print0 | xargs -0 | \sed 's/\/Users\/siva\/.marks\///g')
+    else
+      local wordlist=$(find $MARKPATH -type l -printf "%f\n")
+    fi
+    COMPREPLY=($(compgen -W '${wordlist[@]}' -- "$curw"))
+    return 0
 }
 
 complete -F _completemarks jump unmark
