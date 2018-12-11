fb1f77b8c4f6bd952f989d740aacccc839e3ca26
siva.kommuri <snv.kommuri@gmail.com>
2018-12-10 16:32:08 -0800

    Update git, fish, vim files

--- ansible/fish/files/custom.fish
+++ ansible/fish/files/custom.fish
@@ -2,10 +2,9 @@
 fish_vi_key_bindings
 
 # Conda files
-source /Users/siva.kommuri/.local/share/omf/themes/sushi/fish_right_prompt.fish
-source /usr/local/anaconda/etc/fish/conf.d/conda.fish
-
-conda activate base
+# source /Users/siva.kommuri/.local/share/omf/themes/sushi/fish_right_prompt.fish
+# source /usr/local/anaconda/etc/fish/conf.d/conda.fish
+# conda activate base
 
 set -gx PATH /usr/local/opt/mysql-client/bin $PATH
 
@@ -13,6 +12,38 @@ set -gx GOPATH $HOME/go
 
 set -gx PATH $GOPATH/bin $PATH
 alias go colorgo
+alias git hub
 
 set -gx LESS "-XgmR"
+
+# bobthefish
+set -g theme_color_scheme  light
+set -g theme_display_k8s_context yes
+set -g theme_display_virtualenv no
+
+###################################
+
+# https://stackoverflow.com/questions/4421633/who-is-listening-on-a-given-tcp-port-on-mac-os-x
+function pport
+  lsof -nP -iTCP:$argv | grep LISTEN
+end
+
+# alias get-aws-credentials='f(){ cmd="docker run -it --rm -v ~/.aws:/package/.aws quay.io/wework/onelogin-aws-saml sh -c \"python /package/samlapi.py "$@"\""; bash -c "${cmd}" unset -f f; }; f'
+function get-aws-credentials
+	docker run -it --rm -v ~/.aws:/package/.aws quay.io/wework/onelogin-aws-saml sh -c "python /package/samlapi.py $argv" 
+end
+
+
+# kube - config management
+# kubectx - context management
+# kubens - namespace management
+function kube
+	if count $argv > /dev/null
+		# switch to the specified config
+		set -gx KUBECONFIG ~/.kube/$argv
+	else
+		# list kube configs
+		grep -le '^contexts:' ~/.kube/* 2> /dev/null | cut -d '/' -f 5
+	end
+end
 
--- ansible/fish/files/right_prompt.fish
+++ ansible/fish/files/right_prompt.fish
@@ -1,22 +1,37 @@
-if functions -q fish_right_prompt
-    functions -e __fish_right_prompt
-    functions -c fish_right_prompt __fish_right_prompt
-    functions -e fish_right_prompt
-else
-    function __fish_right_prompt_orig
-    end
-end
-
 function __kubectl_context_add_prompt
     set_color normal
     echo -n '('
     set_color -o green
     echo -n (kubectl config current-context)
     set_color normal
-    echo -n ') '
+    echo -n ')'
 end
 
-function fish_right_prompt
-    __kubectl_context_add_prompt
-    __fish_right_prompt
+# if functions -q fish_right_prompt
+#     functions -e __fish_right_prompt
+#     functions -c fish_right_prompt __fish_right_prompt
+#     functions -e fish_right_prompt
+# else
+#     function __fish_right_prompt_orig
+#     end
+# end
+
+# function fish_right_prompt
+#     __kubectl_context_add_prompt
+#     __fish_right_prompt
+# end
+
+source /Users/siva.kommuri/.local/share/omf/themes/bobthefish/fish_right_prompt.fish
+
+function fish_right_prompt --description 'bobthefish is all about the right prompt'
+        set -l __bobthefish_left_arrow_glyph \uE0B3
+  if [ "$theme_powerline_fonts" = "no" ]
+    set __bobthefish_left_arrow_glyph '<'
+  end
+
+  set_color $fish_color_autosuggestion
+
+  __bobthefish_cmd_duration
+  __bobthefish_timestamp
+  set_color normal
 end
