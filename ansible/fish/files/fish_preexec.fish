396e10ae43b7a6503c57bab865df1612e62d51f0
siva.kommuri <snv.kommuri@gmail.com>
2020-05-19 22:58:34 -0700

    update

--- ansible/fish/files/fish_preexec.fish
+++ ansible/fish/files/fish_preexec.fish
@@ -1,13 +1,13 @@
 # https://spin.atomicobject.com/2016/05/28/log-bash-history/
 # http://stackoverflow.com/q/38485384/1216965
 
-function log_commands --on-event fish_preexec
-  if [ $argv ]
-    mkdir -p ~/.logs
+# function log_commands --on-event fish_preexec
+#   if [ $argv ]
+#     mkdir -p ~/.logs
     # fish_preexec functions receive the commandline as the argument (see `function --help`)
-    echo (date "+%Y-%m-%d %H:%M:%S") (pwd) $argv >> ~/.logs/history-(date "+%Y-%m-%d").log
-  end
-end
+    # echo (date "+%Y-%m-%d %H:%M:%S") (pwd) $argv >> ~/.logs/history-(date "+%Y-%m-%d").log
+    # end
+    # end
 
 #function log_commands_1 --on-event fish_postexec
 #  set prev_cmd_exit $status
