fe2b325824dede8906dc0ac2a985c09a22d9db4e
Siva Kommuri <snv.kommuri@gmail.com>
2021-07-06 10:19:47 -0700

    update

--- ansible/fish/files/kubectl.fish
+++ ansible/fish/files/kubectl.fish
@@ -9,6 +9,9 @@ set -gx PATH $PATH $HOME/.krew/bin
 # kubens - namespace management
 function kube
 	if count $argv > /dev/null
+        # alternatively configure the kubeconfig on the cmdline like so:
+        #       alias kc="kubectl --kubeconfig=sv.kubeconfig"
+
 		# switch to the specified config
 		set -gx KUBECONFIG ~/.kube/$argv
 	else
@@ -41,10 +44,15 @@ end
 
 alias k "kubectl"
 alias kc "k config get-contexts | awk '{print $1}'"
+
 alias kg "k get" 
+alias kgw "k get -o wide --show-labels" 
+alias kgy "k get -o yaml" 
+
 alias kd "k describe" 
 alias ke "k edit" 
 alias kl "k logs -f --tail=100"
+alias kpf "k port-forward"
 
 alias kdump "k cluster-info dump -A --output-directory=/tmp/dump & pushd /tmp/dump"
 
@@ -52,9 +60,32 @@ alias kclear "k config unset current-context"
 alias klist  "k config get-contexts"
 alias kdel   "k config delete-context"
 
+alias kubuntu "k run ubuntu --image=ubuntu:latest --rm -it --restart=Never --command --"
+alias kcurl "k run curl --image=curlimages/curl:latest --rm -it --restart=Never --command -- sh"
+alias ksleep "k apply -f https://raw.githubusercontent.com/istio/istio/master/samples/sleep/sleep.yaml"
+
 alias dc "docker-compose"
 alias dcb "docker-compose build --parallel --compress --force-rm --no-cache --pull"
 alias dcd "docker-compose down --remove-orphans -v"
 alias dcu "docker-compose up --build"
 
-alias bb "k run busybox --image=busybox:latest --rm -it --restart=Never --overrides='{ \"apiVersion\": \"v1\", \"metadata\": {\"annotations\": { \"sidecar.istio.io/inject\":\"false\" } } }' --command --"
+# https://istio.tetratelabs.io/getistio-cli/reference/getistio/
+alias istioctl "getistio istioctl"
+alias i "getistio istioctl"
+
+alias ic "i pc clusters"        # -o json --fqdn reviews.default.svc.cluster.local
+alias il "i pc listeners"       # -o json --address 0.0.0.0 --port 9080
+alias ir "i pc routes"          # -o json --name 9080
+alias ie "i pc endpoints"       # --cluster "outbound|9080||reviews.default.svc.cluster.local"
+alias ib "i pc bootstrap"
+
+# istio debug
+alias ictrlz "i dashboard controlz deployment/istiod.istio-system"
+alias idebug "open http://localhost:8080/debug ; k -n istio-system port-forward deploy/istiod 8080"
+# https://github.com/istio/istio/blob/25f4ef492781382b2da58bff5f3620c48e06cfce/pilot/pkg/xds/debug.go#L151
+#   curl -sS istiod.istio-system:15014/debug/endpointz
+
+# pilot metrics/debug endpoint - https://istio.io/latest/docs/reference/commands/pilot-discovery/#pilot-discovery-request
+#   pilot-agent request GET server_info --log_as_json
+
+alias pilotrestart "k -n istio-system delete po -l app=istiod"
